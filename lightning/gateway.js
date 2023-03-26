6b3f1f68c16ac4598d07816e71d5b5272190619b
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-26 08:05:58 -0300

    Update gateway.js

--- lightning/gateway.js
+++ lightning/gateway.js
@@ -1,3 +1,39 @@
-import lightning
-import lightning 
-import lightning
+const lightningCharge = require('lightning-charge');
+const hyperbee = require('hyperbee');
+
+// Create a Lightning Network node
+const charge = lightningCharge(process.env.LIGHTNING_CHARGE_API_KEY, {
+  host: 'localhost',
+  port: 9112,
+});
+
+// Create a Hyperbee database
+const db = hyperbee('./mydb.hyperbee');
+
+// Listen for Lightning Network payment requests
+charge.stream().on('invoice', (invoice) => {
+  // Store the invoice data in the Hyperbee database
+  const key = `invoice_${invoice.id}`;
+  const value = JSON.stringify(invoice);
+  db.put(key, value);
+});
+
+// Listen for Hyperbee database requests
+app.get('/invoice/:id', async (req, res) => {
+  // Retrieve the invoice data from the Hyperbee database
+  const key = `invoice_${req.params.id}`;
+  const value = await db.get(key);
+  
+  // Create a Lightning Network invoice for the requested data
+  const invoice = await charge.invoice({
+    currency: 'USD',
+    amount: 100,
+    metadata: {
+      hyperbee_key: key,
+    },
+  });
+
+  // Return the Lightning Network invoice to the client
+  res.json({invoice});
+});
+
