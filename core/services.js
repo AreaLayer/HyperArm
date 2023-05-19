158fbe47e6c678a53e13660ac5731785d8f72a1d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-19 08:39:24 -0300

    Create services.js

---
+++ core/services.js
@@ -0,0 +1,31 @@
+const Hyperbee = require('@holepunch/hyperbee')
+const Hypercore = require('@holepunch/hypercore')
+const Hyperdrive = require('@holepunch/hyperdrive')
+const Hyperswarm = require('@holepunch/hyperswarm
+const { promisify } = require('util')
+const crypto = require('crypto')
+const express = require('express')
+const app = express()
+const api = require('coin')
+
+// Create a new Hypercore instance
+const feed = new Hypercore('./bitcoin-pairs-feed', {
+  valueEncoding: 'coin.json'
+  secretKey: crypto.randomBytes(32),
+})
+
+// Create a new Hyperbee instance
+const db = new Hyperbee(feed, {
+  keyEncoding: 'utf-8',
+  valueEncoding: 'con.json'
+})
+//create a new Hyperdrive instance
+const db = new Hyperdrive(feed, {
+  keyEncoding: 'utf-8',
+  valueEncoding: 'co.json'
+})
+//create a new Hyperswarm instance
+const db = new Hyperswarm(feed, {
+  KeyEndocing: 'utf-8',
+  valueEncoding: 'co.json'
+})
