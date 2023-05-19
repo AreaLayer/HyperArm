1f90ef298b6b40bb053b2f75360326510220b41d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-19 08:38:34 -0300

    Create pairs.js

---
+++ core/pairs.js
@@ -0,0 +1,34 @@
+const pairs = [
+  {
+    id: 1,
+    base: 'BTC',
+    quote: 'USD',
+    price: 22436,
+  },
+  {
+    id: 2,
+    base: 'BTC',
+    quote: 'EUR',
+    price: 21000,
+  },
+  {
+    id: 3,
+    base: 'BTC',
+    quote: 'GBP',
+    price: 18332,
+  },
+  {
+    id: 4
+    base: 'BTC'
+    quote: 'BRL'
+    price: 100000,
+  }
+  ],
+
+async function addPairsToDb() {
+  for (const pair of pairs) {
+    await db.put(pair.id.toString(), pair)
+  }
+}
+
+addPairsToDb()
