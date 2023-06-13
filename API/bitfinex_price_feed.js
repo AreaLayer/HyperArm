f40af2c5db083f971fe5d8e8646af1257771fc1e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-13 13:44:55 -0300

    Update bitfinex_price_feed.js

--- API/bitfinex_price_feed.js
+++ API/bitfinex_price_feed.js
@@ -1,1 +1,13 @@
+const axios = require('axios') // Axios library for Node
+
+const baseUrl = "https://api-pub.bitfinex.com/v2/"; // Domain
+const pathParams = "tickers" // Change based on relevant path params listed in the documentation
+const queryParams = "symbols=fUSD,tBTCUSD" // Change based on relevant query params listed in the documentation
+
+axios.get(`${baseUrl}/${pathParams}?${queryParams}`)
+    .then(response => {
+        console.log(response.data); // Logs response data
+    }, error => {
+        console.log(error); // Catches and logs any error
+    })
 
