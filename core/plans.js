8b6ba7041dc140b36c33ef6a74899040b2b4ae6d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-09 07:36:21 -0300

    Update plans.js

--- core/plans.js
+++ core/plans.js
@@ -1,9 +1,35 @@
-const plans = require('hyperarm');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-class plan 1 = {
-  plan 1: 
+const app = express();
+const port = 3000;
 
-class plan 2 = {
+mongoose.connect('mongodb://localhost:27017/subscription_plans', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-class plan 3 = {
+const subscriptionPlanSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+});
 
+const SubscriptionPlan = mongoose.model('SubscriptionPlan', subscriptionPlanSchema);
+
+app.use(bodyParser.json());
+
+app.post('/subscription-plans', async (req, res) => {
+  try {
+    const subscriptionPlan = new SubscriptionPlan(req.body);
+    await subscriptionPlan.save();
+    res.status(201).send(subscriptionPlan);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
